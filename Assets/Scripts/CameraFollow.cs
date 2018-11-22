e3a8c63dc75a9d8ee659e813d9d6707348951f7a
cdunham927 <cdunham927@gmail.com>
2018-11-22 13:50:11 -0800

    Camera Follow Basic

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float moveSpd = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        SetCamera();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * moveSpd);
+	}
+
+    void SetCamera()
+    {
+        transform.LookAt(target.position);
+        offset = transform.position - target.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D bod;
+    Animator anim;
+    public float spd;
+    Vector2 input;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
